869f6961f780e5211becb8c9f8c5564e5041f261
emr <renquin@awtce.be>
2019-03-22 07:53:33 +0100

    Episode 4: items

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+
+    #region Singleton
+
+    public static Inventory instance;    
+
+    void Awake()
+    {
+        if (instance != null) {
+            Debug.LogWarning("More than 1 instance of inventory");
+            return;
+        }
+
+        instance = this;
+    }
+
+    #endregion
+
+    public delegate void OnItemChanged();
+    public OnItemChanged onItemChangedCallback;
+
+    public int space = 20;
+
+    public List<Item> items = new List<Item>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool Add(Item item) {
+
+        if (!item.isDefaultItem) {
+            if (items.Count >= space) {
+                Debug.Log("Not enough room");
+                return false;
+            }
+
+            items.Add(item);
+
+            if (onItemChangedCallback != null) {
+                onItemChangedCallback.Invoke();
+            }
+        }
+
+        return true;
+    }
+
+    public void Remove(Item item)
+    {
+        items.Remove(item);
+
+        if (onItemChangedCallback != null)
+        {
+            onItemChangedCallback.Invoke();
+        }
+    }
+}
