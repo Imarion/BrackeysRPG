34e0190c3e6323e81c5f0d70a4729b63bb0afea8
emr <renquin@awtce.be>
2019-03-26 08:34:21 +0100

    Ep08 working well up to Blendshape setup at 12:20

--- Assets/Scripts/EquipmentManager.cs
+++ Assets/Scripts/EquipmentManager.cs
@@ -22,7 +22,9 @@ public class EquipmentManager : MonoBehaviour
 
     #endregion
 
+    public SkinnedMeshRenderer targetMesh;
     Equipment[] currentEquipment;
+    SkinnedMeshRenderer[] currentMeshes;
 
     public delegate void OnEquipmentChanged(Equipment newItem, Equipment oldItem);
     public OnEquipmentChanged onEquipmentChanged;
@@ -36,6 +38,7 @@ public class EquipmentManager : MonoBehaviour
 
         int numSlots = System.Enum.GetNames(typeof(EquipmentSlot)).Length;
         currentEquipment = new Equipment[numSlots];
+        currentMeshes = new SkinnedMeshRenderer[numSlots];
     }
 
     // Update is called once per frame
@@ -60,14 +63,26 @@ public class EquipmentManager : MonoBehaviour
             onEquipmentChanged.Invoke(newItem, oldItem);
         }
 
+        SetEquipmentBlendShapes(newItem, 100);
+
         currentEquipment[slotIndex] = newItem;
+
+        SkinnedMeshRenderer newMesh = Instantiate<SkinnedMeshRenderer>(newItem.mesh);
+        newMesh.transform.parent = targetMesh.transform;
+        newMesh.bones = targetMesh.bones;
+        newMesh.rootBone = targetMesh.rootBone;
+        currentMeshes[slotIndex] = newMesh;
     }
 
     public void Unequip(int slotIndex) {        
 
         if (currentEquipment[slotIndex] != null)
         {
+            if (currentMeshes[slotIndex] != null) {
+                Destroy(currentMeshes[slotIndex].gameObject);
+            }
             Equipment oldItem = currentEquipment[slotIndex];
+            SetEquipmentBlendShapes(oldItem, 0);
             inventory.Add(oldItem);
             currentEquipment[slotIndex] = null;
 
@@ -83,6 +98,12 @@ public class EquipmentManager : MonoBehaviour
         for (int i = 0; i < currentEquipment.Length; i++)
         {
             Unequip(i);
+        }
+    }
+
+    void SetEquipmentBlendShapes(Equipment item, int weight) {
+        foreach (EquipmentMeshRegion blendSape in item.coveredMeshRegions) {
+            targetMesh.SetBlendShapeWeight((int)blendSape, weight);
         }
     }
 }
