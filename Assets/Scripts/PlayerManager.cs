36a14beb845cf11c2a566f21ae2fa5f6641f0968
emr <renquin@awtce.be>
2019-03-27 13:52:27 +0100

    Episode 10: enemy AI

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    #region Singleton
+
+    public static PlayerManager instance;
+
+    private void Awake()
+    {
+
+        if (instance != null)
+        {
+            Debug.LogWarning("More than 1 instance of player manager");
+            return;
+        }
+
+        instance = this;
+    }
+
+    #endregion
+
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
