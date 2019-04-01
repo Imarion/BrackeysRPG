3737c9e7f8b73c0650b5bfc60463d99dda5400e6
emr <renquin@awtce.be>
2019-04-01 10:02:53 +0200

    e12_integ3: up to grip animation at 10:29

--- Assets/Scripts/CharacterAnimator.cs
+++ Assets/Scripts/CharacterAnimator.cs
@@ -8,17 +8,17 @@ public class CharacterAnimator : MonoBehaviour
     const float locomotionAnimationSmoothTime = .1f;
 
     NavMeshAgent agent;
-    Animator animator;
+    protected Animator animator;
 
     // Start is called before the first frame update
-    void Start()
+    protected virtual void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         animator = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
-    void Update()
+    protected virtual void Update()
     {
         float speedPercent = agent.velocity.magnitude / agent.speed;
         animator.SetFloat("speedPercent", speedPercent, locomotionAnimationSmoothTime, Time.deltaTime);
